7cd0d09850ee1b36e3bacb7f2b2a9a625a755f4d
rendyrey <rendyreynaldy@gmail.com>
2019-11-20 14:54:34 +0700

    t

---
+++ nodejs_mongodb/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// import routes for the product
+const product = require('./routes/product.route');
+// initialize our express app
+const app = express();
+
+// set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://localhost:27017/products';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoDB, {useNewUrlParser: true,useUnifiedTopology:true}).then(
+  () => { console.log('Database is connected') },
+  err => { console.log('Can not connect to the database' + err) }
+);
+// let db = mongoose.connection;
+// db.on('error',console.error.bind(console,'MongoDB connection error:'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/products', product);
+
+
+const port = 1234;
+
+app.listen(port, () => {
+    console.log("Server is up and running on port number "+port);
+});
---
+++ nodejs_mongodb/controllers/product.controller.js
@@ -0,0 +1,56 @@
+const Product = require('../models/product.model');
+
+// simple version, without validation or sanitation
+// exports.test = function(req,res){
+//     res.send('Greetings from the test controller!');
+// }
+
+// exports.product_create = function(req,res){
+//     let product = new Product(
+//         {
+//             name:req.body.name,
+//             price:req.body.price,
+//         }
+//     );
+
+//     product.save(function(err){
+//         if(err){
+//             return next(err);
+//         }
+//         res.send('Product Created Successfully');
+//     })
+// }
+
+// atau bisa kayak gini
+module.exports = {
+    test:(req,res) => {
+        res.send('Greetings from the test controller!');
+    },
+    product_create:(req,res) => {
+        let product = new Product(
+            {
+                name:req.body.name,
+                price:req.body.price
+            }
+        );
+        product.save(err => {
+            if(err){
+                return next(err);
+            }
+            res.send('Product Created Successfully');
+        });
+    },
+    product_details:(req,res) => {
+        Product.findById(req.params.id,(err,product)=>{
+            if(err) return next(err);
+            res.send(product);
+        });
+    },
+    product_update:(req,res)=>{
+        Product.findByIdAndUpdate(req.params.id,{$set:req.body},function(err,product){
+            if(err) return next(err);
+            res.send('Product updated.');
+        });
+    }
+
+}
