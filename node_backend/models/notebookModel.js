bb6ffffc0d0ee0b73e1dc8dcd56fc05f5a89149f
rendyrey <rendyreynaldy@gmail.com>
2019-12-02 10:34:38 +0700

    test

---
+++ node_backend/models/notebookModel.js
@@ -0,0 +1,22 @@
+import mongoose, {
+    Schema
+} from 'mongoose';
+
+// create database scheme for notes
+
+const NoteScheme = new Schema({
+    title:{
+        type:String,
+        required: "What is the note's title?"
+    },
+    text:{
+        type:String,
+        required: "What is the note?"
+    },
+    date:{
+        type:Date,
+        default: new Date
+    }
+});
+
+export default mongoose.model('Note',NoteScheme);
